579e7f72119a7691e81d85100f7be04aa0943475
kushaank30 <kushaankchawla3006@gmail.com>
2020-05-10 23:15:58 +0530

    Date added

--- javascript.js
+++ javascript.js
@@ -18,3 +18,47 @@ setInterval(() => {
 
 
 })
+
+
+
+var month = document.getElementById('month')
+var date = document.getElementById('date')
+var year = document.getElementById('year')
+var day = document.getElementById('day')
+
+var weekdays = new Array(7)
+weekdays[0] ="Sunday"
+weekdays[1] ="Monday"
+weekdays[2] ="Tuesday"
+weekdays[3] ="Wednesday"
+weekdays[4] ="Thursday"
+weekdays[5] ="Friday"
+weekdays[6] ="Saturday"
+
+var Months = new Array(12)
+Months[1] = "January"
+Months[2] = "February"
+Months[3] = "March"
+Months[4] = "April"
+Months[5] = "May"
+Months[6] = "June"
+Months[7] = "July"
+Months[8] = "August"
+Months[9] = "September"
+Months[10] = "October"
+Months[11] = "November"
+Months[12] = "December"
+
+month.innerHTML = Months[new Date().getMonth() + 1]
+date.innerHTML = new Date().getDate()
+year.innerHTML = new Date().getFullYear()
+day.innerHTML = weekdays[new Date().getDay()]
+
+if(month.innerHTML.toString().length==1){
+    month.innerHTML = "0" + month.innerHTML
+}
+
+if(date.innerHTML.toString().length==1){
+    date.innerHTML = "0" + date.innerHTML
+}
+
